ba491f7edb9281ffa3ab1571d573d58ae24625fe
ericmbouwe <ericmbouwe@yahoo.fr>
2020-09-09 09:33:59 +0100

    fixed linters errors

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ import { updateTempUnit, populateContent } from './modules/dom';
   getAllData('london')
     .then(data => {
       populateContent(data);
-    })
+    });
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(success);
